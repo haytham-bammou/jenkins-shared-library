65e8fb9dae3284e1ceb74da4b0f975c766d8ccf5
haytham-bammou <Haythambm86@gmail.com>
2022-08-15 21:33:57 +0100

    adding helper classes

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,5 @@
-def call(String imageName) {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh "docker build -t $imageName ."
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh "docker push $imageName"
-    }
-}
+import com.example.Docker
 
-// haythambammou/my-repo:jma-2.0
+def call(String imageName) {
+    return new Docker(this).buildImage(imageName)
+}
