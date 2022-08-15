08f425c62d7a5edb857d89672d80c5cad1573265
haytham-bammou <Haythambm86@gmail.com>
2022-08-15 21:05:41 +0100

    parameters

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,9 +1,10 @@
-def call() {
+def call(String imageName) {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t haythambammou/my-repo:jma-2.0 .'
+        sh "docker build -t $imageName ."
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push haythambammou/my-repo:jma-2.0'
+        sh "docker push $imageName"
     }
 }
 
+// haythambammou/my-repo:jma-2.0
